e4f3b6d8194d4a8e6c1afc061f183451b61d73b9
Lucas de Paula <lucasdafonsecadepaula@gmail.com>
2024-09-05 13:34:03 -0300

    Adicionando algoritmo basico de sorting, Merge sort

---
+++ 2- Algorithms/1- Basic/Sorting/Merge.cpp
@@ -0,0 +1,79 @@
+#include <iostream>
+using namespace std;
+
+// Merge two subarrays L and R into arr
+void merge(int arr[], int l, int m, int r) {
+    int n1 = m - l + 1;
+    int n2 = r - m;
+
+    // Create temporary arrays
+    int L[n1], R[n2];
+
+    // Copy data to temp arrays L[] and R[]
+    for (int i = 0; i < n1; i++)
+        L[i] = arr[l + i];
+    for (int j = 0; j < n2; j++)
+        R[j] = arr[m + 1 + j];
+
+    // Merge the temp arrays back into arr[l..r]
+    int i = 0, j = 0, k = l;
+    while (i < n1 && j < n2) {
+        if (L[i] <= R[j]) {
+            arr[k] = L[i];
+            i++;
+        } else {
+            arr[k] = R[j];
+            j++;
+        }
+        k++;
+    }
+
+    // Copy the remaining elements of L[], if any
+    while (i < n1) {
+        arr[k] = L[i];
+        i++;
+        k++;
+    }
+
+    // Copy the remaining elements of R[], if any
+    while (j < n2) {
+        arr[k] = R[j];
+        j++;
+        k++;
+    }
+}
+
+// l is for left index and r is right index of the sub-array
+// of arr to be sorted
+void mergeSort(int arr[], int l, int r) {
+    if (l >= r)
+        return;
+
+    int m = l + (r - l) / 2;
+
+    // Recursively sort first and second halves
+    mergeSort(arr, l, m);
+    mergeSort(arr, m + 1, r);
+
+    // Merge the sorted halves
+    merge(arr, l, m, r);
+}
+
+int main() {
+    int arr[] = {38, 27, 43, 3, 9, 82, 10};
+    int arr_size = sizeof(arr) / sizeof(arr[0]);
+
+    cout << "Given array is \n";
+    for (int i = 0; i < arr_size; i++)
+        cout << arr[i] << " ";
+    cout << endl;
+
+    mergeSort(arr, 0, arr_size - 1);
+
+    cout << "Sorted array is \n";
+    for (int i = 0; i < arr_size; i++)
+        cout << arr[i] << " ";
+    cout << endl;
+
+    return 0;
+}
