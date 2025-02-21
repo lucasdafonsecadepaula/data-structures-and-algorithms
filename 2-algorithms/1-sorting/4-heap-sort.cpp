8ba2d05bef1ac031eec3f58fa91d886006055202
Lucas de Paula <lucasdafonsecadepaula@gmail.com>
2025-02-21 00:14:28 -0300

    Sorting Algorithms Implementation: Better testing for heap sort;

--- 2-algorithms/1-sorting/4-heap-sort.cpp
+++ 2-algorithms/1-sorting/4-heap-sort.cpp
@@ -1,7 +1,6 @@
 #include <iostream>
 #include <cassert>
 #include <vector>
-
 using namespace std;
 
 int getParent(int index)
@@ -57,20 +56,62 @@ void heapSort(vector<int> &arr)
     }
 }
 
-void testHeapSort(vector<int> &arr)
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+
+void testHeapSort()
 {
-    heapSort(arr);
-    vector<int> expected = {1, 2, 3, 4, 5, 6, 7, 8, 9};
-    assert(arr == expected);
+    vector<int> arr1 = {5, 2, 9, 3, 1, 4, 8, 6, 7};
+    heapSort(arr1);
+    assert(arr1 == vector<int>({1, 2, 3, 4, 5, 6, 7, 8, 9}));
+
+    vector<int> arr2 = {1, 2, 3, 4, 5, 6, 7, 8, 9};
+    heapSort(arr2);
+    assert(arr2 == vector<int>({1, 2, 3, 4, 5, 6, 7, 8, 9}));
+
+    vector<int> arr3 = {9, 8, 7, 6, 5, 4, 3, 2, 1};
+    heapSort(arr3);
+    assert(arr3 == vector<int>({1, 2, 3, 4, 5, 6, 7, 8, 9}));
+
+    vector<int> arr4 = {5, 5, 5, 5, 5};
+    heapSort(arr4);
+    assert(arr4 == vector<int>({5, 5, 5, 5, 5}));
+
+    vector<int> arr5 = {42};
+    heapSort(arr5);
+    assert(arr5 == vector<int>({42}));
+
+    vector<int> arr6 = {};
+    heapSort(arr6);
+    assert(arr6.empty());
+
+    vector<int> arr7 = {-3, -1, -4, -2, 0, 2, 1};
+    heapSort(arr7);
+    assert(arr7 == vector<int>({-4, -3, -2, -1, 0, 1, 2}));
+
+    cout << "All heap sort tests passed!" << endl;
 }
 
 int main()
 {
-    vector<int> arr = {5, 2, 9, 3, 1, 4, 8, 6, 7};
-
-    testHeapSort(arr);
-
-    cout << "Heap sort test passed!" << endl;
-
+    testHeapSort();
     return 0;
 }
